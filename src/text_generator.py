f32763b221fd6d8a090b79f0717cbfc9a0ec21bd
Nic <ingersonmeacham01@allegheny.edu>
2023-05-02 11:24:08 -0400

    fix: Update API key in text_generator.py

--- src/text_generator.py
+++ src/text_generator.py
@@ -1,9 +1,9 @@
 import openai
 
 openai.organization = 'org-tiQjuNLO4GEneutRBVq6wVx6'
-openai.api_key = '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'
+openai.api_key = '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'
 
-def generate_text(prompt, model, temperature=0.5, max_tokens=100):
+def generate_text(prompt, model, temperature=0.5, max_tokens=712):
     response = openai.Completion.create(
       engine=model,
       prompt=prompt,
@@ -22,7 +22,7 @@ def generate_text(prompt, model, temperature=0.5, max_tokens=100):
 prompt = "Congratulations on your election victory! As a newly elected politician, you have a unique opportunity to make a difference in the lives of your constituents. Write a speech to inspire them and outline your vision for the future."
 model = "text-davinci-002"
 
-generated_text = generate_text(prompt, model, temperature=0.5, max_tokens=100)
+generated_text = generate_text(prompt, model, temperature=0.5, max_tokens=712)
 print(generated_text)
 
 with open('speech.txt', 'w') as f:
